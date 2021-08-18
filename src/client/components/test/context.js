8d0812a402a6be74f6bad52110450c9dc5d74a64
r-e-l <ariel@hiredscore.com>
2021-08-18 17:58:07 +0300

    better-identify-resource-type

--- src/client/components/test/context.js
+++ src/client/components/test/context.js
@@ -7,8 +7,8 @@ import styles from './test.css';
 
 const cx = classNames.bind(styles);
 
-const videoRegEx = /(?:mp4|webm)$/i;
-const imgRegEx = /(?:png|jpe?g|gif)$/i;
+const videoRegEx = /(?:mp4|webm)$|(?:mp4|webm)\?/i;
+const imgRegEx = /(?:png|jpe?g|gif)$|(?:png|jpe?g|gif)\?/i;
 const protocolRegEx = /^(?:(?:https?|ftp):\/\/)/i;
 const urlRegEx = /^(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/; // eslint-disable-line
 const base64ImgRegEx = /^data:image\/([a-zA-Z0-9-_.])+;base64,([^"]*)$/i;
